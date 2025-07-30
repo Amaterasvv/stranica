345289b10bcf1b36e921ddcc42e38fe2e694f33b
Amaterasvv <ktbobas@gmail.com>
2025-07-30 03:38:06 +0200

    asd

--- fade.js
+++ fade.js
@@ -13,3 +13,12 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+
+document.querySelectorAll('.video-wrapper').forEach(wrapper => {
+  wrapper.addEventListener('click', () => {
+    const url = wrapper.getAttribute('data-url');
+    if (url) window.location.href = url;
+  });
+});
+
